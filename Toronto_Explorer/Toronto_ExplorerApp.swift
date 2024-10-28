e8497cd3997a6aeff3b6cab4f2fd5186c1513931
Jeet Panchal <jeetbpanchal@gmail.com>
2024-10-28 13:54:02 -0400

    Initial Commit

---
+++ Toronto_Explorer/Toronto_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toronto_ExplorerApp.swift
+//  Toronto_Explorer
+//
+//  Created by Jeet Panchal on 2024-10-28.
+//
+
+import SwiftUI
+
+@main
+struct Toronto_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Toronto_ExplorerTests/Toronto_ExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Toronto_ExplorerTests.swift
+//  Toronto_ExplorerTests
+//
+//  Created by Jeet Panchal on 2024-10-28.
+//
+
+import Testing
+@testable import Toronto_Explorer
+
+struct Toronto_ExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
